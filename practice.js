d36367efae9fbe8ed399e7475a09a5e293fd30b0
8221117 <sainandhu1998@gmail.com>
2023-02-01 23:53:36 +0530

    Contact form

---
+++ practice.js
@@ -0,0 +1,37 @@
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("mouseover", mouseOverfn);
+
+function mouseOverfn(e) {
+  e.preventDefault();
+  btn.style.background = "pink";
+  // document.querySelector(".btn").classList.add("sam");
+}
+
+//const btn2 = document.querySelector(".btn");
+
+btn.addEventListener("mouseout", mouseoutfn);
+
+function mouseoutfn(e) {
+  //e.preventDefault();
+  //btn2.classList.add("btn");
+  btn.style.background = "";
+  //document.querySelector(".btn").classList.add("nandhu");
+}
+
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+myForm.addEventListener("click", onClick);
+
+function onClick(e) {
+  e.preventDefault();
+  if (nameInput.value === "" || emailInput.value === "") {
+    msg.classList.add("error");
+    msg.innerHTML = "Please enter all fields";
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    console.log("success");
+  }
+}

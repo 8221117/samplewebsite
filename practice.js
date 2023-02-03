fcb915c35a34fac4cd5481ae7f79a3461a3c5506
8221117 <sainandhu1998@gmail.com>
2023-02-03 23:09:15 +0530

    added delete button and delete func

--- practice.js
+++ practice.js
@@ -45,22 +45,47 @@ function onClick(e) {
 
     //console.log(userDetails);
     let userDetails_serialized = JSON.stringify(userDetails);
-    localStorage.setItem("user", userDetails_serialized);
+    localStorage.setItem(userDetails.getuserMail, userDetails_serialized);
     showOnscreen(userDetails);
 
     let userDetails_deserialized = JSON.parse(localStorage.getItem("user")); // to get back the user details originally as object form.
     console.log(userDetails_deserialized);
 
     // show the user data on the screen .
-
+    //const parentEle = document.getElementById("users");
     function showOnscreen(userDetails) {
       const parentEle = document.getElementById("users");
       const childEle = document.createElement("li");
+      childEle.className = "list-items";
       childEle.textContent =
         userDetails.getuserName + "-" + userDetails.getuserMail;
+      // CREATING DELETE BUTTON
+      const delButton = document.createElement("input");
+      delButton.className = "deletebtn delete";
+      delButton.type = "button";
+      delButton.value = "Delete";
+      //adding text node to delete element
+      //delEle.appendChild(document.createTextNode("delete"));
+      delButton.onclick = () => {
+        localStorage.removeItem(userDetails.getuserMail);
+        // localStorage.removeItem(userDetails.getuserName);
+        // localStorage.getItem(userDetails.getuserName);
+        parentEle.removeChild(childEle);
+      };
+      childEle.appendChild(delButton);
       parentEle.appendChild(childEle);
-    }
-    /** wrong method var p = document.createElement("p");
+      //parentEle.addEventListener("click", removeli);
+      // REMOVE LI on clicking delete button
+      /*function removeli(e) {
+        //e.preventDefault();
+        if (e.target.classList.contains("delete")) {
+          if (confirm("Are you sure")) {
+            var childEle = e.target.parentElement;
+            parentEle.removeChild(childEle);
+          }
+        }*/
+
+      /** wrong method var p = document.createElement("p");
     p.textContent = userDetails.getuserName + "-" + userDetails.getuserMail;
 
     var li = document.createElement("li");
@@ -69,5 +94,6 @@ function onClick(e) {
     li.appendChild(p);
 
     console.log(li);**/
+    }
   }
 }
